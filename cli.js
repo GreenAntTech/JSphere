f8c61c51893684ad4c9cd5c77d77158de2ba011d
Yohan <yohan.moore@greenantsolutions.com>
2025-03-21 14:09:27 -0400

    Version: v1.0.0-preview.18

--- cli.js
+++ cli.js
@@ -941,7 +941,7 @@ const LF = "\n";
 const CRLF = "\r\n";
 Deno?.build.os === "windows" ? CRLF : LF;
 const cmdArgs = parse1(Deno.args);
-const JSPHERE_VERSION = 'v1.0.0-preview.17';
+const JSPHERE_VERSION = 'v1.0.0-preview.18';
 const DENO_VERSION = '@DENO_VERSION';
 (async function() {
     try {
