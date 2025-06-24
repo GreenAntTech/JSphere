93ba0c197523844d1525063eac6969b8510e1463
DigitalEmergence <yohanmooredev@gmail.com>
2025-06-24 12:00:51 -0400

    Version: v1.0.0-preview.72

--- server.js
+++ server.js
@@ -23393,12 +23393,12 @@ class Utils {
         return encString;
     };
 }
-const version = 'v1.0.0-preview.71';
+const version = 'v1.0.0-preview.72';
 const denoVersion = '2.2.4';
-let project = {};
+const project = {};
 async function init1(config) {
     for(const key in currentConfig)Deno.env.delete(key);
-    project = {};
+    for(const key in project)delete project[key];
     if (typeof config == 'object') {
         currentConfig = config;
         for(const key in currentConfig){
--- shared/element.js
+++ shared/element.js
@@ -1,4 +1,4 @@
-console.log('elementJS:', 'v1.0.0-preview.71');
+console.log('elementJS:', 'v1.0.0-preview.72');
 const appContext = {
     server: globalThis.Deno ? true : false,
     client: globalThis.Deno ? false : true,
