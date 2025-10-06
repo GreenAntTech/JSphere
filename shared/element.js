60038cbe3012751780ae9205a856db6d56e4acd3
DigitalEmergence <yohanmooredev@gmail.com>
2025-10-06 11:39:00 -0400

    Version: v1.0.0-preview.150

--- shared/element.js
+++ shared/element.js
@@ -1,4 +1,4 @@
-console.log('elementJS:', 'v1.0.0-preview.149');
+console.log('elementJS:', 'v1.0.0-preview.150');
 const appContext = {
     server: globalThis.Deno ? true : false,
     client: globalThis.Deno ? false : true,
